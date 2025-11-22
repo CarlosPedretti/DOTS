569d456e121eec2f92af6b00f2e0b8f6f2b50c92
CarlosPedretti <112439911+CarlosPedretti@users.noreply.github.com>
2025-11-22 18:04:39 -0300

    FrustrumCulling

---
+++ Assets/Scripts/Frustum Culling/Scripts/Editor/FrustumCullingCustomInspector.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Utilities
+{
+    [CustomEditor(typeof(FrustumCulling))]
+    public class FrustumCullingCustomInspector : Editor
+    {
+        SerializedProperty autoCatchCamera,
+        mainCam,
+        gameView,
+        activationDirection,
+        xRangeTwoD,
+        
+        runEveryFrames,
+        restingFrames,
+
+        rotationMaxSpeed,
+        minRestFrames,
+        maxRestFrames,
+
+
+        cullInScene,
+        preventCullingCloseObjects,
+        closeObjectsDistance,
+
+        distanceCulling,
+        distanceToCull,
+        prioritizeDistanceCulling,
+        distanceCullingOnly;
+
+        FrustumCulling script;
+        
+
+        void OnEnable()
+        {
+            autoCatchCamera = serializedObject.FindProperty("autoCatchCamera");
+            mainCam = serializedObject.FindProperty("mainCam");
+            gameView = serializedObject.FindProperty("gameView");
+            activationDirection = serializedObject.FindProperty("activationDirection");
+            xRangeTwoD = serializedObject.FindProperty("xRangeTwoD");
+
+            runEveryFrames = serializedObject.FindProperty("runEveryFrames");
+
+            rotationMaxSpeed = serializedObject.FindProperty("rotationMaxSpeed");
+
+
+            cullInScene = serializedObject.FindProperty("cullInScene");
+            preventCullingCloseObjects = serializedObject.FindProperty("preventCullingCloseObjects");
+            closeObjectsDistance = serializedObject.FindProperty("closeObjectsDistance");
+            
+            distanceCulling = serializedObject.FindProperty("distanceCulling");
+            distanceToCull = serializedObject.FindProperty("distanceToCull");
+            prioritizeDistanceCulling = serializedObject.FindProperty("prioritizeDistanceCulling");
+            distanceCullingOnly = serializedObject.FindProperty("distanceCullingOnly");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update ();
+
+            FrustumCulling script = (FrustumCulling) target;
+            
+            
+            EditorGUILayout.LabelField("Camera Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(autoCatchCamera);
+            if (script.autoCatchCamera == false) {
+                EditorGUILayout.PropertyField(mainCam);
+            }
+            EditorGUILayout.PropertyField(gameView);
+            EditorGUILayout.PropertyField(activationDirection);
+            if (script.gameView == FrustumCulling.GameViewOption.TwoD) {
+                EditorGUILayout.PropertyField(xRangeTwoD);
+            }
+            EditorGUILayout.Space(10);
+
+
+            EditorGUILayout.LabelField("Performance Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(runEveryFrames);
+            EditorGUILayout.PropertyField(rotationMaxSpeed);
+            EditorGUILayout.Space(10);
+
+            EditorGUILayout.LabelField("Objects Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(cullInScene);
+            EditorGUILayout.PropertyField(preventCullingCloseObjects);
+            if (script.preventCullingCloseObjects) {
+                EditorGUILayout.PropertyField(closeObjectsDistance);
+            }
+            EditorGUILayout.Space(10);
+
+            EditorGUILayout.LabelField("Distance Settings", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(distanceCulling);
+            
+            EditorGUI.BeginDisabledGroup(script.distanceCulling == false);
+                EditorGUILayout.PropertyField(distanceToCull);
+                
+                EditorGUI.BeginDisabledGroup(script.distanceCullingOnly == true);
+                    EditorGUILayout.PropertyField(prioritizeDistanceCulling);
+                EditorGUI.EndDisabledGroup ();
+                
+                EditorGUILayout.PropertyField(distanceCullingOnly);
+            EditorGUI.EndDisabledGroup ();
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
